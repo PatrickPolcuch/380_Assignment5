e130b8518a0c61e8d1148904f101dcedddd3620e
PatrickPolcuch <polcuch@chapman.edu>
2023-04-11 10:44:16 -0700

    fixed some errors, included new libraries

--- prodcon.c
+++ prodcon.c
@@ -1,6 +1,8 @@
 
 #include "buffer.h"
 #include <pthread.h>
+#include <unistd.h> //for sleep()
+#include <stdio. h>
 
 
 int main(int argc, char *argv[]){
@@ -15,15 +17,15 @@ int main(int argc, char *argv[]){
 
     //make buffer
 
-    if(create_producer_theads(num_producers); < 0){
+    if(create_producer_theads(num_producers) < 0){
         return -1;
     }
 
-    if(create_consumer_threads(num_consumers); <0){
+    if(create_consumer_threads(num_consumers) <0){
         return -1;
     }
 
-    //sleep
+    sleep(delay);
 
     return 0;
 
@@ -34,7 +36,7 @@ int create_producer_theads(int numThreads){
         pthread_t producer_id;
         void *producer_args;
 
-        int producer_result = pthread_create(&producer_id, NULL, *producer, producer_args);
+        int producer_result = pthread_create(&producer_id, NULL, *producer(), producer_args);
 
         if(producer_result != 0){
             printf("Failed to create producer.");
@@ -50,7 +52,7 @@ int create_consumer_threads(int numThreads){
         pthread_t consumer_id;
         void *consumer_args;
 
-        int consumer_result = pthread_create(&consumer_id, NULL, *consumer, consumer_args);
+        int consumer_result = pthread_create(&consumer_id, NULL, *consumer(), consumer_args);
 
         if(consumer_result != 0){
             printf("Failed to create consumer.");
@@ -66,26 +68,26 @@ void *producer(){
         //
         //produce an item in next_produced
         //
-        wait(empty);
-        wait(mutex);
+        //wait(empty);
+        //wait(mutex);
         //
         //add next_produced to the buffer
         //
-        signal(mutex);
-        signal(full);
+        //signal(mutex);
+        //signal(full);
     }
     return NULL;
 }
 
 void *consumer(){
     while(1){
-        wait(full);
-        wait(mutex);
+        //wait(full);
+        //wait(mutex);
         //
         //remove an item from buffer to next_consumed
         //
-        signal(mutex);
-        signal(empty);
+        //signal(mutex);
+        //signal(empty);
         //
         //consume the item in next_consumed
         //
