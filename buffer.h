e50d75b9016fcc241a832bee92338560237e68a2
PatrickPolcuch <polcuch@chapman.edu>
2023-04-12 12:50:23 -0700

    this time for sure

--- buffer.h
+++ buffer.h
@@ -2,10 +2,11 @@
 #include <stdint.h>
 
 #define BUFFER_SIZE 30
-struct buffer_item{
+struct buffer_item {
     uint16_t cksum;
     uint8_t buffer[BUFFER_SIZE];
 };
+typedef struct buffer_item buffer_item;
 
 int insert_item(buffer_item item);
 int remove_item(buffer_item *item);
